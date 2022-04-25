2bc596ad03b23cb4d2d1ad1de4d691dd8ac236b9
BineetKarmakar <83464472+BineetKarmakar@users.noreply.github.com>
2022-04-25 21:59:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+difference=0
+left_wrist_x=0
+right_wrist_x=0
+
 function setup(){
     video=createCapture(VIDEO)
     video.size(550,500)
@@ -16,5 +20,18 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results)
+        left_wrist_x = results[0].pose.leftWrist.x
+        right_wrist_x = results[0].pose.rightWrist.x
+
+        difference = floor(left_wrist_x - right_wrist_x)
+        console.log("left_wrist_x=",left_wrist_x,"right_wrist_x=",right_wrist_x,"difference=",difference)
     }
+}
+
+function draw(){
+    background('#969A97')
+    fill('#F90093')
+    textSize(difference)
+    text('Bineet',50,400)
+
 }
